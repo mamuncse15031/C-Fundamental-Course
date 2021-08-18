86b62825a7b208af7a5cd22e836894f1cff051fb
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-08-18 11:44:36 +0600

    variable 2

--- Variable/Variable/Program.cs
+++ Variable/Variable/Program.cs
@@ -7,6 +7,13 @@ namespace Variable
         static void Main(string[] args)
         {
             byte number=2;
+            int count = 10;
+            float totalPrice = 10.54f;
+            decimal price = 114.5m;
+            char name = 'A';
+            string fullName = "Mamun";
+            bool isWorking = false;
+            
             Console.WriteLine(number);
         }
     }
