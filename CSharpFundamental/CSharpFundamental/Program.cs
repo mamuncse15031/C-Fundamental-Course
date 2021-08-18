a3a6e1e6aed483ba4d5537e64b8659e805a02f0e
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-08-18 21:39:45 +0600

    Classes and Objects

---
+++ CSharpFundamental/CSharpFundamental/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using CSharpFundamental.Math;
+
+namespace CSharpFundamental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mamun = new Person();
+            mamun.FirstName = "Mamun";
+            mamun.LastName = "Shuvo";
+            mamun.Introduce();
+
+            var calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+        
+    }
+}
