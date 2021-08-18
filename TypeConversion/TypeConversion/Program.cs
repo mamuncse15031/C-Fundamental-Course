ac450c0f45003d9fe204e9ad29fd2d37cc25bb62
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-08-18 19:53:46 +0600

    Compatible conversion

--- TypeConversion/TypeConversion/Program.cs
+++ TypeConversion/TypeConversion/Program.cs
@@ -4,17 +4,52 @@ namespace TypeConversion
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            //Implicit Conversion
             byte b = 1;
             int i = b;
 
             Console.WriteLine(value: i);
 
+            //Explicit Conversion
             int ii = 1;
             byte bb = (byte)ii;
 
             Console.WriteLine(value: bb);
+
+            //Compatible conversion
+            string str = "1234";
+            int a = Convert.ToInt32(str);
+
+            Console.WriteLine(value: a);
+
+            //Non-Compatible Conversion (Exception Handling)
+            try
+            {
+                string strrr = "1000";
+                byte c = Convert.ToByte(strrr);
+
+                Console.WriteLine(c);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The conversion is not possible!!!");
+            }
+
+
+            try
+            {
+                string st = "True";
+                bool d = Convert.ToBoolean(st);
+
+                Console.WriteLine(d);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The conversion is not possible!!!");
+            }
+            
 
         }
     }
