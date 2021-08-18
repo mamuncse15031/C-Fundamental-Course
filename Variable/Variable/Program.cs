c7658e1d9c3720148ca81bf8db70e86969e7718c
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-08-18 09:40:02 +0600

    variable Scope

---
+++ Variable/Variable/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number=2;
+            Console.WriteLine(number);
+        }
+    }
+}
