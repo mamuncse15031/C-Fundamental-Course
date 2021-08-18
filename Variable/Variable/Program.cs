eb338cd0bf02e941be2b58caffee38bdda13344e
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-08-18 12:29:10 +0600

    ...

--- Variable/Variable/Program.cs
+++ Variable/Variable/Program.cs
@@ -6,21 +6,9 @@ namespace Variable
     {
         static void Main(string[] args)
         {
-            var number=2;
-            var count = 10;
-            var totalPrice = 10.54f;
-            var price = 114.5m;
-            var name = 'A';
-            var fullName = "Mamun";
-            var isWorking = false;
-            
-            Console.WriteLine(number);
-            Console.WriteLine(count);
-            Console.WriteLine(totalPrice);
-            Console.WriteLine(price);
-            Console.WriteLine(name);
-            Console.WriteLine(fullName);
-            Console.WriteLine(isWorking);
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue );
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
         }
     }
 }
