8be30a06ff84a5d2707b850e4f663af2c0bb8e07
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-08-18 19:31:22 +0600

    Implicit and Explicit Type Conversion

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 1;
+            int i = b;
+
+            Console.WriteLine(value: i);
+
+            int ii = 1;
+            byte bb = (byte)ii;
+
+            Console.WriteLine(value: bb);
+
+        }
+    }
+}
